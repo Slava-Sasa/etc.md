f3baf7238b6884fb2a37f88bba95c68839df4f85
pirus <veaceslav.bodistean@gmail.com>
2020-04-27 00:00:33 -0700

    Initial commit

---
+++ common/config/main.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        //..
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'cache' => 'cache' //Включаем кеширование
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '' => 'site/index',
+                '<_a:[\w\-]+>' => 'site/<_a>',
+            ],
+        ],
+    ],
+];
